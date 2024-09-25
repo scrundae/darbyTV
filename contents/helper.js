a81549bdfdf627f00821f207094d5bcb26e876df
Harry Collins <121592185+scrundae@users.noreply.github.com>
2024-09-25 15:45:30 +1000

    Holy moly

---
+++ contents/helper.js
@@ -0,0 +1,4 @@
+document.getElementById('content').innerHTML = `
+    <h1>Under-construction</h1>
+    <p>Please come back soon.</p>
+`;
---
+++ contents/index.js
@@ -0,0 +1,5 @@
+document.getElementById('content').innerHTML = `
+    <h1>Welcome Home</h1>
+    <p>Welcome to DarbyTV! Pick an option in "The Picker" to access your favourite content.</p>
+    <img src="bootcamp.png">
+`;
