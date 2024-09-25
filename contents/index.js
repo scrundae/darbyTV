bc46dc36609b7a4b23acca1f21c1ec469d54955c
Harry Collins <Local>
2024-09-25 17:16:45 +1000

    Add pages for finder and darwin

--- contents/index.js
+++ contents/index.js
@@ -1,5 +1,5 @@
 document.getElementById('content').innerHTML = `
     <h1>Welcome Home</h1>
     <p>Welcome to DarbyTV! Pick an option in "The Picker" to access your favourite content.</p>
-    <img src="bootcamp.png">
+    <img class="pspecial" src="bootcamp.png">
 `;
